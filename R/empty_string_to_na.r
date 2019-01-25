ce10429e4d9cb9b29dd1ed2e76ea2ca9efc15557
Markus Dumke <markusdumke@gmail.com>
2019-01-25 23:47:20 +0100

    update docs

--- R/empty_string_to_na.r
+++ R/empty_string_to_na.r
@@ -1,11 +1,11 @@
-#' Convert empty string to NA
+#' Convert empty string and "NA" to NA
 #'
 #' @param .x Character vector.
 #'
 #' @return Vector of same length as `.x`.
 #'
 #' @examples
-#' empty_string_to_na(c("a", "", "", "b"))
+#' empty_string_to_na(c("a", "", "NA", "b"))
 empty_string_to_na <- function(.x) {
   .x[.x == ""] <- NA_character_
   .x[.x == "NA"] <- NA_character_
